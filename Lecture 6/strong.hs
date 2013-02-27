eaefefc638ce1ba9699a15e05ae3897c980f4345
Derek Mahar <derek.mahar@intelerad.com>
2013-02-26 20:34:20 -0500

    Add function strong2 which uses function "any" instead of "foldl".

--- Lecture 6/strong.hs
+++ Lecture 6/strong.hs
@@ -1,7 +1,14 @@
 import Data.Char
+import Data.List
 
 strong :: String -> Bool
 strong password = length password >= 15
 	&& foldl (\acc c -> acc || isUpper c) False password
 	&& foldl (\acc c -> acc || isLower c) False password
 	&& foldl (\acc c -> acc || isNumber c) False password
+
+strong2 :: String -> Bool
+strong2 password = length password >= 15
+	&& any isUpper password
+	&& any isLower password
+	&& any isNumber password
